bb38a070a27337ca6572b7086819bc9bec2decd9
Nikola Lemmens <nikolalemmens@icloud.com>
2019-07-18 10:08:32 +0200

    Added Project.

---
+++ CreatingARWithVuforia/Assets/Scripts/CustomTrackableEventHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomTrackableEventHandler : DefaultTrackableEventHandler
+{
+    PlayOnStateChange playOnStateChange;
+
+    private void Awake()
+    {
+        playOnStateChange = GetComponentInChildren<PlayOnStateChange>();
+    }
+
+    protected override void OnTrackingFound()
+
+    {
+        base.OnTrackingFound();
+        playOnStateChange.Init();
+        playOnStateChange.PlayOnAppear();
+    }
+
+    protected override void OnTrackingLost()
+    {
+        base.OnTrackingLost();
+        playOnStateChange.PlayOnDisappear();
+    }
+
+
+
+
+}
---
+++ CreatingARWithVuforia/Assets/Scripts/PlayOnStateChange.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayOnStateChange : MonoBehaviour
+{
+	
+		public AudioClip appearSound;
+		public AudioClip disappearSound;
+
+		private AudioSource audioSource;
+
+		void Awake()
+		{
+			audioSource = GetComponent<AudioSource>();
+			audioSource.enabled = false;
+
+
+		}
+
+
+		public void Init()
+		{
+			audioSource.enabled = true; ;
+		}
+
+		public void PlayOnAppear()
+		{
+			audioSource.clip = appearSound;
+			audioSource.Play();
+		}
+
+		public void PlayOnDisappear()
+		{
+			audioSource.clip = disappearSound;
+			audioSource.Play();
+		}
+
+}
